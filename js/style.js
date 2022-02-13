d86ba996ccb024a9c0324ea5fded87e01cbe71ef
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2022-02-13 18:08:50 +0200

    Update style.js

--- js/style.js
+++ js/style.js
@@ -16,38 +16,4 @@ for(let i=0; i<button.length; i++){
 };
 
 
-// form
-// let Name= document.getElementById('name'),
-//     email= document.getElementById('email'),
-//     subject= document.getElementById('subject'),
-//     message= document.getElementsByTagName('textarea'),
-//     submit= document.getElementById('submit');
-
-// let label1= document.getElementById('lab1'),
-//     label2= document.getElementById('lab2'),
-//     label3= document.getElementById('lab3'),
-//     label4= document.getElementById('lab4');
-
-
-//     submit.onclick= function(){
-    
-//         label1.textContent= Name.value;
-//         label2.textContent= email.value;
-//         label3.textContent= subject.value;
-//         label4.textContent= message[0].value;
-// }
-
-
-let inputs= document.getElementsByTagName('input'),
-    message= document.getElementById('message'),
-    labels= document.getElementsByClassName('lab'),
-    submit= document.getElementById('submit');
-
-    submit.onclick= function(){
-    
-        for(let i=0; i<labels.length-1; i++){
-            labels[i].textContent= inputs[i].value;
-        }
-        labels[3].textContent= message.value;
-}
 
